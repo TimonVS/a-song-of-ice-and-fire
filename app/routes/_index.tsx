443fd1f2e46d3a6e1854d94ae39beaba84d65c8b
Timon van Spronsen <timonvanspronsen@outlook.com>
2023-04-05 16:51:39 +0200

    Move /houses to index

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,38 +1,78 @@
-import type { V2_MetaFunction } from "@remix-run/cloudflare";
+import {
+  type LoaderArgs,
+  type V2_MetaFunction,
+  type HeadersFunction,
+  json,
+} from "@remix-run/cloudflare";
+import { Link, useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+import { getHouses } from "~/lib/asoiaf-api";
+
+const buttonClasses = "border rounded-lg bg-gray-100 p-2";
 
 export const meta: V2_MetaFunction = () => {
-  return [{ title: "New Remix App" }];
+  return [{ title: "A Song of Ice and Fire — overview" }];
 };
 
 export default function Index() {
+  const { houses, currentPage, hasNextPage, hasPrevPage } =
+    useLoaderData<typeof loader>();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+    <div>
+      <h2 className="text-3xl mb-4">Houses</h2>
+      <ul className="list-disc pl-4 mb-8 space-y-1">
+        {houses.map((house) => (
+          <li key={house.url}>
+            <Link to={`/houses/${house.id}`} className="font-semibold">
+              {house.name}
+            </Link>
+          </li>
+        ))}
       </ul>
+      <nav>
+        <ul className="font-semibold space-x-4">
+          {hasPrevPage && (
+            <li className="inline">
+              <Link to={`/?page=${currentPage - 1}`} className={buttonClasses}>
+                ← Previous
+              </Link>
+            </li>
+          )}
+          {hasNextPage && (
+            <li className="inline">
+              <Link to={`/?page=${currentPage + 1}`} className={buttonClasses}>
+                Next →
+              </Link>
+            </li>
+          )}
+        </ul>
+      </nav>
     </div>
   );
 }
+
+export async function loader({ request }: LoaderArgs) {
+  const url = new URL(request.url);
+  const rawPage = url.searchParams.get("page");
+  const page = rawPage ? parseInt(rawPage, 10) : 1;
+  invariant(!Number.isNaN(page), "page must be of type number");
+
+  return json(
+    {
+      currentPage: page,
+      ...(await getHouses({ page })),
+    },
+    {
+      headers: {
+        "Cache-Control": "public, max-age=86400",
+      },
+    }
+  );
+}
+
+export const headers: HeadersFunction = ({ loaderHeaders }) => {
+  return {
+    "Cache-Control": loaderHeaders.get("Cache-Control")!,
+  };
+};
